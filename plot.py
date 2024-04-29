db4a25c99ccc46a1777e0258adcbcf1fe57e3828
Yier Lin <157223366+random1106@users.noreply.github.com>
2024-04-29 15:03:29 -0500

    Update plot.py
    The size of the figure is changed.

--- plot.py
+++ plot.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import bisect
 
-#WindowSize
+# windowSize
 xMin=0;xMax=12
 yMin=0;yMax=12
 
-#Choose the q parameter in [0,1]
+# choose the q parameter in [0,1]
 
 def plotTPNG(q):
   time = 0
@@ -17,48 +17,36 @@ def plotTPNG(q):
     iters += 1
     oldTime = time
     time += np.random.exponential(scale=1/(xMax - xMin), size=None)
-    #print("time", time)
-    #move old particle up to new time
+
+    # move old particle up to new time (draw vertical segment)
     for i in range(len(particleLocations)):
-      # plt.plot([particleLocations[i], particleLocations[i]], [oldTime, time], 'k')
       lines_to_plot.append([particleLocations[i], particleLocations[i]]) # x
       lines_to_plot.append([oldTime, time]) # y
-      lines_to_plot.append('#000000')# format
+      lines_to_plot.append('k') 
 
-    #add new particle at location
+    # add new particle at location
     location = np.random.uniform(low=xMin, high=xMax, size=None)
-    #print("location", location)
 
     index = bisect.bisect(particleLocations, location)
     particleLocations.insert(index, location)
-    # particleLocations.append(location)
-    # particleLocations.sort()
-    # index = particleLocations.index(location)
 
-    #sample geometric distribution
+    # sample geometric distribution
     if q < 1:
       numJumps = np.random.geometric(1-q, size=None)
     elif q == 1:
       numJumps = len(particleLocations) - index
-    #plt.scatter(location,time, color= '#0000ff')
     if index + numJumps > len(particleLocations) - 1:
       endPoint= xMax
     else:
       endPoint = particleLocations[index+ numJumps]
       particleLocations.remove(endPoint)
-
+    # draw horizontal segment
     lines_to_plot.append([location,endPoint]) # x
     lines_to_plot.append([time, time]) # y
-    lines_to_plot.append('k') # format
-    #plt.plot([location,endPoint],[time,time], 'k')
+    lines_to_plot.append('k')
   return lines_to_plot
 
-#   plt.plot(*lines_to_plot)
-
-# plt.xlabel("x"); plt.ylabel("y")
-# plt.axis('equal')
-
-fig, axs = plt.subplots(2, 2)
+fig, axs = plt.subplots(2, 2, figsize=(10, 10))
 axs[0, 0].plot(*plotTPNG(0))
 axs[0, 0].set_title('t=0')
 axs[0, 1].plot(*plotTPNG(.4))
@@ -74,10 +62,5 @@ for ax in axs.flat:
     plt.rcParams["figure.figsize"] = (20,20)
     ax.set_xlim(xMin, xMax)
     ax.set_ylim(yMin, yMax)
-# plt.axis('equal')
-# Hide x labels and tick labels for top plots and y ticks for right plots.
-for ax in axs.flat:
-    ax.label_outer()
 
 plt.show()
-
